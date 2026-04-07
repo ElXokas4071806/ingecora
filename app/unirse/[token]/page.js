8679d779ded4d3f4cfe35e5b1fb758fc5e6b83e4
Tomas Bernal <bernaltomas22@gmail.com>
2026-04-07 17:39:13 -0500

    mejoras responsive

--- app/unirse/[token]/page.js
+++ app/unirse/[token]/page.js
@@ -1,6 +1,6 @@
 'use client'
 import { useState, useEffect } from 'react'
-import { createClient } from '../../../lib/supabase'
+import { createClient } from '@/lib/supabase'
 import { useRouter, useParams } from 'next/navigation'
 import { HardHat, CheckCircle, XCircle } from 'lucide-react'
 
